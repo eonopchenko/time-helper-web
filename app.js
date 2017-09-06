4a295a772e9e5801a0350f47b6a1d8c90b378875
eonopchenko <jake.spb@gmail.com>
2017-09-07 11:23:25 +1200

    views -> public

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ if (cfenv.getAppEnv().isLocal) {
 }
 */
 
-app.use(express.static("views"));
+app.use(express.static("public"));
 
 app.get('/',function(req,res){
-	res.sendfile(__dirname + '/views/index.html');
+	res.sendfile(__dirname + '/public/index.html');
 });
 
 var appEnv = cfenv.getAppEnv();
-app.listen(appEnv.port, '127.0.0.1', function() {
+app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
 });
