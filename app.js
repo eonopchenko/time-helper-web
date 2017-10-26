ce668e28ddf2019ebac46b6d470ebf5a4e3e957e
eonopchenko <jake.spb@gmail.com>
2017-10-26 16:10:41 +1300

    ~Refactored +Added update, remove actions +Added user permissions reading +Added markup styles

--- app.js
+++ app.js
@@ -15,103 +15,129 @@ const Notification = bluemix_push_notifications.Notification;
 const LOGIN_URL = "/ibm/bluemix/appid/login";
 const CALLBACK_URL = "/ibm/bluemix/appid/callback";
 
-if (cfenv.getAppEnv().isLocal) {
-   console.error('This sample should not work locally, please push the sample to Bluemix.');
-   process.exit(1);
-}
-
-app.use(helmet());
-app.use(helmet.noCache());
-app.enable("trust proxy");
-app.use(express_enforces_ssl());
-
-app.use(session({
-  secret: "123456",
-  resave: true,
-  saveUninitialized: true,
-	proxy: true,
-	cookie: {
-		httpOnly: true,
-		secure: true
-	}
-}));
-
 app.set('view engine', 'ejs');
 
 app.use(express.static("views"));
 
-app.use(passport.initialize());
-app.use(passport.session());
+var name = "";
+var email = "";
+var picture = "";
 
-passport.use(new bluemix_appid.WebAppStrategy());
-
-bluemix_appid.UserAttributeManager.init();
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-app.get(LOGIN_URL, passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME, {
-  forceLogin: true
-}));
-
-app.get(CALLBACK_URL, passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME, {allowAnonymousLogin: true}));
-
-app.get('/',function(req,res) {
-	res.sendfile(__dirname + '/views/index.html');
-});
-
-app.get("/protected", passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME), function(req, res) {
-  var accessToken = req.session[bluemix_appid.WebAppStrategy.AUTH_CONTEXT].accessToken;
-
-  bluemix_appid.UserAttributeManager.getAllAttributes(accessToken).then(function (attributes) {
-    res.render('landing', {
-        name: req.user.name, 
-        email: req.user.email,
-        picture: req.user.picture
-    });
-  });
-});
-
-app.get("/login", passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME, {successRedirect : '/protected', forceLogin: true}));
-
-
-///--- CLOUDANT DATABASE CONNECTION ---///
+///--- CLOUDANT SETTINGS ---///
 var cloudant_url = 'https://dd99ab4a-3be7-4c60-9eea-90e610e7ff3b-bluemix:397e251ab62bc131be9b552263a174e7d6701f4c261f8f0ed55853e4867e6b00@dd99ab4a-3be7-4c60-9eea-90e610e7ff3b-bluemix.cloudant.com';
 var cloudant_instance = сloudant({url: cloudant_url});
 db = cloudant_instance.db.use('timetable_db');
 
-app.get('/read_classes',function(req, res) {
-  var url = cloudant_url + "/timetable_db/_design/timetable_db/_view/timetable_db";
-	request({
-      url: url,
-      json: true
-    }, function (error, response, body) {
+///--- LOCAL LOGIN ---///
+if (cfenv.getAppEnv().isLocal) {
+  app.get('/login',function(req,res) {
+    name = "User";
+    email = "user@mail.com";
+    picture = "https://cdn1.iconfinder.com/data/icons/mix-color-4/502/Untitled-1-512.png";
+    var permissions_db = cloudant_instance.db.use('user_permissions_db');
+    permissions_db.get(email, function(err, data) {
+      if(data) {
+        res.render('landing', {
+          name: name, 
+          email: email,
+          picture: picture
+        });
+      }
+    });
+  });
+}
+
+///--- REMOTE LOGIN ---///
+else {
+  app.use(helmet());
+  app.use(helmet.noCache());
+  app.enable("trust proxy");
+  app.use(express_enforces_ssl());
+  app.use(session({
+    secret: "123456",
+    resave: true,
+    saveUninitialized: true,
+    proxy: true,
+    cookie: {
+      httpOnly: true,
+      secure: true
+    }
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  passport.use(new bluemix_appid.WebAppStrategy());
+  
+  bluemix_appid.UserAttributeManager.init();
+  
+  passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
+  
+  app.get(LOGIN_URL, passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME, {
+    forceLogin: true
+  }));
+  
+  app.get(CALLBACK_URL, passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME, {allowAnonymousLogin: true}));
+  
+  app.get("/protected", passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME), function(req, res) {
+    var accessToken = req.session[bluemix_appid.WebAppStrategy.AUTH_CONTEXT].accessToken;
+  
+    bluemix_appid.UserAttributeManager.getAllAttributes(accessToken).then(function (attributes) {
+      name = req.user.name;
+      email = req.user.email;
+      picture = req.user.picture;
+      res.render('landing', {
+          name: name, 
+          email: email,
+          picture: picture
+      });
+    });
+  });
+  
+  app.get("/login", passport.authenticate(bluemix_appid.WebAppStrategy.STRATEGY_NAME, {successRedirect : '/protected', forceLogin: true}));
+}
+
+app.get('/',function(req,res) {
+  res.sendfile(__dirname + '/views/index.html');
+});
+
+app.get('/fill_remove_update_classes_dropdown', function(req, res) {
+	var url = cloudant_url + "/timetable_db/_design/timetable_db/_view/timetable_db";
+  request({
+    url: url, 
+    json: true
+  }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var user_data = body.rows;
       var list_of_classes = '[';
-      var days_array = [];
-      var start_time_array = [];
-      var end_time_array = [];
+      var start_times_array = [];
+      var durations_array = [];
       var titles_array = [];
+      var descriptions_array = [];
 
       for(var i = 0; i < user_data.length; i++) {
-        days_array.push(user_data[i].value[1]);
-        start_time_array.push(user_data[i].value[2]);
-        end_time_array.push(user_data[i].value[3]);
-        titles_array.push(user_data[i].value[4]);
+        if(user_data[i].value[5] == email) {
+          start_times_array.push(user_data[i].value[1]);
+          durations_array.push(user_data[i].value[2]);
+          titles_array.push(user_data[i].value[3]);
+          descriptions_array.push(user_data[i].value[4]);
+        }
       }
 
       for(var i = 0; i < titles_array.length; i++) {
         var json_block = 
-          '{\"day\":\"' + days_array[i] + '\",' + 
-          '\"start_time\":\"' + start_time_array[i] + '\",' + 
-          '\"end_time\":\"' + end_time_array[i] + '\",' + 
-          '\"title\":\"' + titles_array[i] + '\"}';
+        '{' + 
+        '\"start\":\"' + start_times_array[i] + '\",' + 
+        '\"duration\":\"' + durations_array[i] + '\",' + 
+        '\"title\":\"' + titles_array[i] + '\",' + 
+        '\"description\":\"' + descriptions_array[i] + '\"' + 
+        '}';
         if(i !== 0) {
           list_of_classes = list_of_classes.concat(",");
         }
@@ -121,48 +147,178 @@ app.get('/read_classes',function(req, res) {
       list_of_classes = list_of_classes.concat("]");
       res.contentType('application/json');
       res.send(JSON.parse(list_of_classes));
-		} else {
-			var str ="{\"error\":\"db read error\"}";
-			res.contentType('application/json');
-			res.send(JSON.parse(str));
-		}
-	});
+    } else {
+      console.log("No data from URL");
+      console.log("Response is : " + response.statusCode);
+      var class_string="{\"added\":\"DB read error\"}";
+      res.contentType('application/json');
+      res.send(JSON.parse(class_string));
+    }
+  });
+});
+
+app.get('/account',function(req,res) {
+  res.render('account', {
+      name: name, 
+      email: email,
+      picture: picture
+  });
 });
 
 app.get('/create_class',function(req, res) {
   var url = cloudant_url + "/timetable_db/_design/timetable_db/_view/timetable_db";
-  var name_string;
-  
+  var title_string;
   request({
     url: url,
     json: true
   }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
+      console.log("Recived: " + JSON.stringify(req.query));
+      req.query.user = email;
       db.insert(req.query, function(err, data) {
         if (!err) {
-          name_string="{\"added\":\"Yes\"}";
+          title_string="{\"added\":\"Yes\"}";
 
           var pushNotification = new PushNotifications(PushNotifications.Region.SYDNEY, "1e1125cd-32ed-4e96-bb35-ab62cb806322", "a5c93e43-91f2-405d-bbc0-2691a8fdbaaf");
           var notification = new Notification("New class has been scheduled: " + 
-            req.query.day + ", " + 
-            req.query.start_time + ", " + 
-            req.query.end_time + ", " + 
-            req.query.title);
+            req.query.start + ", " + 
+            req.query.duration + ", " + 
+            req.query.title + ", " + 
+            req.query.description);
           pushNotification.send(notification, function(error, response, body) {
           });
         }
         else {
-          name_string="{\"added\":\"DB insert error\"}";
+          title_string="{\"added\":\"DB insert error\"}";
         }
         res.contentType('application/json');
-        res.send(JSON.parse(name_string));
+        res.send(JSON.parse(title_string));
       });
     }
     else {
-      name_string="{\"added\":\"DB read error\"}";
+      title_string="{\"added\":\"DB read error\"}";
       res.contentType('application/json');
-      res.send(JSON.parse(name_string));
+      res.send(JSON.parse(title_string));
     }
+  });
+});
+
+app.get('/update_class',function(req, res) {
+  console.log("Received : " + JSON.stringify(req.query));
+  var url = cloudant_url + "/timetable_db/_design/timetable_db/_view/timetable_db";
+  request({
+    url: url, 
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var class_string;
+      var user_data = body.rows;
+      var id_to_update;
+      var rev_to_update;
+      var total_rows = user_data.length;
+
+      for (var i = 0; i < user_data.length; i++) {
+         var doc = user_data[i];
+        if (doc.value[1] === req.query.upd_start) {
+          id_to_update = doc.key;
+          rev_to_update = doc.value[0];
+        }
+      }
+
+      var string_to_update = "{" + 
+        "\"_id\":\"" + id_to_update + "\", " + 
+        "\"_rev\":\"" + rev_to_update + "\", " + 
+        "\"start\":\"" + req.query.upd_start + "\", " + 
+        "\"duration\":\"" + req.query.upd_duration + "\", " + 
+        "\"title\":\"" + req.query.upd_class_title + "\", " + 
+        "\"description\":\"" + req.query.upd_class_description + "\"," + 
+        "\"user\":\"" + email + "\"" + 
+        "}";
+
+      var update_obj = JSON.parse(string_to_update);
+      if(total_rows !== 0) {
+        db.insert(update_obj, function(err, data) {
+          if(!err) {
+            console.log("Updated doc.");
+            class_string="{\"updated\":\"updated\"}";
+            res.contentType('application/json');
+            res.send(JSON.parse(class_string));
+          } else {
+            console.log("Couldn't update class " + err);
+            class_string="{\"updated\":\"could not update\"}";
+            res.contentType('application/json');
+            res.send(JSON.parse(class_string));
+          }
+        });
+      } else {
+        console.log("DB is empty");
+        var class_string="{\"updated\":\"empty database\"}";
+        res.contentType('application/json');
+        res.send(JSON.parse(class_string));
+      }
+    } else {
+      console.log("No response from URL. Status : " + response.statusCode);
+      class_string="{\"updated\":\"DB read error\"}";
+      res.contentType('application/json');
+      res.send(JSON.parse(class_string));
+    }
+  });
+});
+
+app.get('/remove_class',function(req, res) {
+  console.log("Class to be removed : = " + req.query.start);
+  var url = cloudant_url + "/timetable_db/_design/timetable_db/_view/timetable_db";
+  request({
+       url: url, 
+       json: true
+      }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var class_string;
+        var user_data = body.rows;
+        var id_to_remove;
+        var rev_to_remove;
+        var total_rows = user_data.length;
+        for(var i=0; i< user_data.length; i++) {
+          var doc = user_data[i];
+          if(doc.value[1] === req.query.start) {
+            id_to_remove = doc.key;
+            rev_to_remove = doc.value[0];
+            break;
+          }
+        }
+        if(total_rows !== 0) {
+          console.log("Removed class");
+          db.destroy(id_to_remove, rev_to_remove, function(err) {
+            if(!err) {
+              console.log("Removed class");
+              class_string="{\"removed\":\"removed\"}";
+              res.contentType('application/json');
+              res.send(JSON.parse(class_string));
+            } else {
+              console.log("Couldn't remove class");
+              console.log(err);
+              class_string="{\"removed\":\"could not remove\"}";
+              res.contentType('application/json');
+              res.send(JSON.parse(class_string));
+            }
+          });
+        }
+        else
+        {
+          console.log("DB is empty");
+          class_string="{\"removed\":\"empty database\"}";
+          res.contentType('application/json');
+          res.send(JSON.parse(class_string));
+        }
+      }
+      else
+      {
+        console.log("No data from URL");
+        console.log("Response is : " + response.statusCode);
+        class_string="{\"removed\":\"DB read error\"}";
+        res.contentType('application/json');
+        res.send(JSON.parse(class_string));
+      }
   });
 });
 
--- views/js/classes_actions.js
+++ views/js/classes_actions.js
@@ -1,44 +1,158 @@
 $(document).ready(function() {
-    $("#read_classes").click(function(e) {
-		e.preventDefault();
-		$.ajax({
-			url: "/read_classes",
-			type: "GET",
-			dataType: "json",
-			data:{allclasses: ""},
-			contentType: "application/json",
-			cache: true,
-			timeout: 5000,
-			complete: function() {
-			  console.log('process complete');
-			},
-			success: function(data) {
-				console.log('process success');
-				document.getElementById("create_class_form").style.display="none";
-				document.getElementById("show_all_classes").style.display="block";
-				var classes_table = "<table>";
-				classes_table += "<tr>";
-				classes_table += "<th>Weekday</th>";
-				classes_table += "<th>Start time</th>";
-				classes_table += "<th>End time</th>";
-				classes_table += "<th>Title</th>";
-				classes_table += "</tr>";
-				for(var i = 0; i < data.length; i++) {
-					classes_table += "<tr>";
-					classes_table += "<td>" + data[i].day + "</td>";
-					classes_table += "<td>" + data[i].start_time + "</td>";
-					classes_table += "<td>" + data[i].end_time + "</td>";
-					classes_table += "<td>" + data[i].title + "</td>";
-					classes_table += "</tr>";
+
+	$.ajax({
+		url: "/fill_remove_update_classes_dropdown",
+		type: "GET",
+		dataType: "json",
+		data:{allclasses: ""},
+		contentType: "application/json",
+		cache: true,
+		timeout: 5000,
+		complete: function() {
+		  console.log('process complete');
+		},
+		success: function(data) {
+			var classes_drop_down;
+			for(var i = 0; i < data.length; i++)
+			{
+				var class_option = '<option>' + data[i].title + '</option>';
+				classes_drop_down += class_option;
+			}
+			document.getElementById("create_class_form").style.display="none";
+
+			for(var i = 0; i < data.length; i++) {
+				var start = moment(data[i].start, "DD/MM/YYYY hh:mm A");
+				var duration = data[i].duration;
+				var sp = duration.split(":");
+				var hours = parseInt(sp[0]);
+				var mins = parseInt(sp[1]);
+
+				var d = start.toDate().getDay() + 1;
+				var m = start.toDate().getMonth() + 1;
+				var y = start.toDate().getYear() + 1900;
+
+				var colIndex = (start.minutes() == 0 ? 1 : 0) + d - 1;
+				var rowIndex = ((start.hours() - 9) * 2 + (start.minutes() == 0 ? 0 : 1)) + 2;
+
+				var cells = hours * 2 + (mins == 0 ? 0 : 1);
+
+				var textColors =       ["#1A4F0A", "#4E4809", "#4F370B", "#0F264F", "#4F0C4A"];
+				var backgroundColors = ["#AAFF98", "#FEF39A", "#FFCC71", "#A6C2FF", "#FFC1F9"];
+				var buttonColors =     ["#A1F48F", "#F3E891", "#F4C36A", "#9DB8F4", "#F4B5EE"];
+
+				/// Assign colors, depending on row numbers
+				var colorIndex = 0;
+				for(var s = 5; s >= 1; s--) {
+					if(((rowIndex - 2) % s) == 0) {
+						colorIndex = s - 1;
+						break;
+					}
 				}
-				classes_table += "</table>";
-				document.getElementById("show_all_classes").innerHTML = classes_table;
-            },
-            error: function() {
-                console.log('process error');
-            },
-        });
+
+				(document.getElementById("taskView").rows[rowIndex].cells[colIndex]).innerHTML = 
+				'<div id="over" style="z-index: 1;height: ' + (cells * 100) + 'px;color: ' + textColors[colorIndex] + ';background: ' + backgroundColors[colorIndex] +'; margin-left: auto; margin-right: auto; display: block;">' + 
+				"<button type=\"button\" onclick=\"" + 
+					"document.getElementById('update_class_form').style.display='block';" + 
+					"document.getElementById('create_class_form').style.display='none';" + 
+					"getElementById('upd_start').value = '" + moment(start).format('DD/MM/YYYY hh:mm A') + "';" + 
+					// "document.getElementById('upd_start').value = '" + data[i].start + "';" + 
+					"document.getElementById('upd_duration').value = '" + data[i].duration + "';" + 
+					"document.getElementById('upd_class_title').value = '" + data[i].title + "';" + 
+					"document.getElementById('upd_class_description').value = '" + data[i].description + "';" + 
+				"\"" + 
+				"class=\"btn\" style=\"background-color:" + backgroundColors[colorIndex] + ";margin-left: 0;margin-right: auto;float: left;\">" + 
+				"<span class='glyphicon glyphicon-edit' aria-hidden='true'></span>" + 
+				"</button>" + 
+				"<button type=\"button\" onclick=" + 
+				"\"" + 
+				"$.ajax({" + 
+					"url: '/remove_class'," + 
+					"type: 'GET'," + 
+					"dataType: 'json'," + 
+					"data: {" + 
+						"start: '" + data[i].start + "'" + 
+					"}," + 
+					"contentType: 'application/json'," + 
+					"cache: true," + 
+					"timeout: 5000," + 
+					"complete: function() {" + 
+						"console.log('process complete');" + 
+					"}," + 
+					"success: function(data) {" + 
+						"console.log('process success');" + 
+						"document.getElementById('removed_class').style.display='block';" + 
+					"}," + 
+					"error: function() {" + 
+						"console.log('process error');" + 
+					"}," + 
+				"});\"" + 
+				"class=\"btn\" style=\"background-color:" + backgroundColors[colorIndex] + ";margin-left: auto;margin-right: 0;float: right;\">" + 
+				"<span class='glyphicon glyphicon-remove' aria-hidden='true'></span>" + 
+				"</button>" + 
+				'<img src="img/pin.png" width="42" height="42">' + 
+				"<h4>" + 
+				"<p><strong># " + data[i].title + "</strong></p>" + 
+				"</h4>" + 
+				"<p style='font-family:\"Comic Sans MS\"'>" + data[i].description + "</p>" + 
+				"</div>";
+
+				// // Calculate the new height
+				// var height = cells * 100;
+				// var cell = (document.getElementById("taskView").rows[rowIndex].cells[colIndex]);
+				// // Create a new div to go inside the cell
+				// var newDiv = document.createElement("div");
+				// newDiv.style.color = textColors[colorIndex];
+				// newDiv.style.background = backgroundColors[colorIndex];
+				// newDiv.style.marginLeft = "auto";
+				// newDiv.style.marginRight = "right";
+				// newDiv.style.display = "block";
+				// newDiv.style.height = height + "px";
+				// newDiv.innerHTML = 
+				// 	'<button type="button" class="btn" style="float: right; background-color:' + buttonColors[colorIndex] + ';">' + 
+				// 	"<span class='glyphicon glyphicon-remove' aria-hidden='true'></span>" + 
+				// 	"</button>" + 
+				// 	'<img src="img/pin.png" width="42" height="42">' + 
+				// 	"<h4>" + 
+				// 	"<p><strong># " + data[i].title + "</strong></p>" + 
+				// 	"</h4>" + 
+				// 	"<p style='font-family:\"Comic Sans MS\"'>" + data[i].description + "</p>";
+				// // Add it to view
+				// cell.appendChild(newDiv);
+			}
+
+			for(var rowIndex = 2; rowIndex < 20; rowIndex++) {
+				var from = ((rowIndex % 2) == 0) ? 1 : 0;
+				var to = ((rowIndex % 2) == 0) ? 8 : 7;
+				for(var colIndex = from; colIndex < to; colIndex++) {
+
+					var hours = Math.floor(((rowIndex - 2) / 2) + 9);
+					var mins = ((rowIndex - 2) % 2 == 0) ? 0 : 30;
+					var dateParts = (document.getElementById("taskView").rows[0].cells[colIndex + ((rowIndex % 2 == 0) ? 0 : 1)]).innerHTML.split("/");
+					var start = new Date(dateParts[2], dateParts[1] - 1, dateParts[0], hours, mins);
+
+					if((document.getElementById("taskView").rows[rowIndex].cells[colIndex]).innerHTML == "&nbsp;") {
+						(document.getElementById("taskView").rows[rowIndex].cells[colIndex]).innerHTML = 
+						"<div>" + 
+						"<button type=\"button\" onclick=\"" + 
+						"document.getElementById('create_class_form').style.display='block';" + 
+						"document.getElementById('update_class_form').style.display='none';" + 
+						"document.getElementById('create_start').value = '" + moment(start).format('DD/MM/YYYY hh:mm A') + "';" + 
+						"\"" + 
+						"class=\"btn\" style=\"background-color: #EDEDEE;width: 100%;height: 99px;\">" + 
+						"<span aria-hidden='true'></span>" + 
+						"</button>" + 
+						"</div>";
+					}
+				}
+			}
+		},
+		error: function() {
+		    console.log('process error');
+		},
 	});
+	Array.prototype.random = function () {
+		return this[Math.floor((Math.random()*this.length))];
+	}
 
 	$("#create_class_button").click(function(e) {
 		e.preventDefault();
@@ -47,10 +161,10 @@ $(document).ready(function() {
 			type: "GET",
 			dataType: "json",
 			data: {
-				day: $("#newday").val(), 
-				start_time: $("#newstart").val(), 
-				end_time: $("#newend").val(), 
-				title: $("#newtitle").val()
+				start: $("#create_start").val(),
+				duration: $("#create_duration").val(),
+				title: $("#create_class_title").val(),
+				description: $("#create_class_description").val()
 			},
 			contentType: "application/json",
 			cache: true,
@@ -62,8 +176,7 @@ $(document).ready(function() {
 				console.log('process success');
 				if(data.added === "Yes") {
 					document.getElementById("create_class_form").style.display="none";
-					document.getElementById("show_all_classes").style.display="none";
-					document.getElementById("created_class").style.display="block";
+	 				document.getElementById("created_class").style.display="block";
 				}
 			},
 			error: function() {
@@ -71,4 +184,31 @@ $(document).ready(function() {
 			},
 		});
 	});
+
+	 $("#update_class_button").click(function(e){
+	 	e.preventDefault();
+	 	$.ajax({
+	 		url: "/update_class",
+	 		type: "GET",
+	 		dataType: "json",
+	 		data: {
+				 upd_start: $("#upd_start").val(), 
+				 upd_duration: $("#upd_duration").val(), 
+				 upd_class_title: $("#upd_class_title").val(), 
+				 upd_class_description: $("#upd_class_description").val()},
+	 		contentType: "application/json",
+	 		cache: true,
+	 		timeout: 5000,
+	 		complete: function() {
+	 			console.log('process complete');
+	 		},
+	 		success: function(data) {
+				console.log('process success');
+	 			if(data.updated === "updated") {
+					document.getElementById("update_class_form").style.display="none";
+	 				document.getElementById("updated_class").style.display="block";
+	 			}
+	 		}
+	 	});
+	 });
 });
